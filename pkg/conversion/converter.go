ef6433244bef25774284de356075750ff0960a51
Andrei Kvapil <kvapss@gmail.com>
2024-10-30 13:54:51 +0100

    add some conversion logic

--- pkg/conversion/converter.go
+++ pkg/conversion/converter.go
@@ -1,6 +1,8 @@
 package conversion
 
 import (
+	"strings"
+
 	helmv2 "github.com/fluxcd/helm-controller/api/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	appsv1alpha1 "k8s.io/sample-apiserver/pkg/apis/apps/v1alpha1"
@@ -11,10 +13,10 @@ func ConvertHelmReleaseToApplication(hr *helmv2.HelmRelease) (*appsv1alpha1.Appl
 	app := &appsv1alpha1.Application{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "apps.cozystack.io/v1alpha1",
-			Kind:       hr.Spec.Chart.Spec.Chart,
+			Kind:       hr.Spec.Chart.Spec.Chart, // TODO: use application.kind
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:              hr.Name,
+			Name:              strings.TrimPrefix(hr.Name, hr.Spec.Chart.Spec.Chart+"-"), // TODO: use release.prefix instead of hr.Spec.Chart.Spec.Chart
 			Namespace:         hr.Namespace,
 			CreationTimestamp: hr.CreationTimestamp,
 			DeletionTimestamp: hr.DeletionTimestamp,
@@ -50,7 +52,7 @@ func ConvertApplicationToHelmRelease(app *appsv1alpha1.Application) (*helmv2.Hel
 			Kind:       "HelmRelease",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      app.Name,
+			Name:      app.Kind + "-" + app.Name, // TODO: use release.prefix instead of kind
 			Namespace: app.Namespace,
 			Labels: map[string]string{
 				"cozystack.io/ui": "true",
@@ -59,7 +61,7 @@ func ConvertApplicationToHelmRelease(app *appsv1alpha1.Application) (*helmv2.Hel
 		Spec: helmv2.HelmReleaseSpec{
 			Chart: &helmv2.HelmChartTemplate{
 				Spec: helmv2.HelmChartTemplateSpec{
-					Chart:             "kubernetes",
+					Chart:             app.Kind, // TODO: use release.chart.name instead of kind
 					Version:           app.AppVersion,
 					ReconcileStrategy: "Revision",
 					SourceRef: helmv2.CrossNamespaceObjectReference{
